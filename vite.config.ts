9430e9d3f05b8ac1118dd39c19c7308c5f27c8c9
parekh-amee <parekhami95@gmail.com>
2024-04-06 19:58:50 +0530

    after deloyment

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fem-sm-dashboard/',
+  base: '/nirvana-lab-assignment/', // Ensure base starts with a slash and ends with a slash
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': '/src', // Update alias path
+    },
+  },
 })
